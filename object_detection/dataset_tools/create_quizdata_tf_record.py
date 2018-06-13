266f66ce4afc2d200abc70b8735698c867e6ce15
zhoujia0309 <1280486226@qq.com>
2018-06-13 11:17:08 +0800

    first commit

---
+++ object_detection/dataset_tools/create_quizdata_tf_record.py
@@ -0,0 +1,266 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+r"""Convert raw PASCAL dataset to TFRecord for object_detection.
+
+Example usage:
+    python object_detection/dataset_tools/create_pascal_tf_record.py \
+        --data_dir=/home/user/VOCdevkit \
+        --year=VOC2012 \
+        --output_path=/home/user/pascal.record
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import hashlib
+import io
+import logging
+import os
+import random
+import re
+
+import numpy as np
+from lxml import etree
+import PIL.Image
+import tensorflow as tf
+
+from object_detection.utils import dataset_util
+from object_detection.utils import label_map_util
+from sklearn.model_selection import train_test_split  
+
+
+#flags = tf.app.flags
+#flags.DEFINE_string('data_dir', '', 'Root directory to raw PASCAL VOC dataset.')
+#flags.DEFINE_string('set', 'train', 'Convert training set, validation set or '
+#                    'merged set.')
+#flags.DEFINE_string('annotations_dir', 'Annotations',
+#                    '(Relative) path to annotations directory.')
+#flags.DEFINE_string('year', 'quiz2018', 'Desired challenge year.')
+#flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
+#flags.DEFINE_string('label_map_path', 'quiz-w8-doc/plabels_items.txt',
+#                    'Path to label map proto')
+#flags.DEFINE_boolean('ignore_difficult_instances', False, 'Whether to ignore '
+#                     'difficult instances')
+#FLAGS = flags.FLAGS
+flags = tf.app.flags
+flags.DEFINE_string('data_dir', '/home/paul/.conda/envs/tensorflow/lib/python3.6/site-packages/tensorflow/models/quiz-w8-data', 'Root directory to raw pet dataset.')
+flags.DEFINE_string('output_dir', '', '')
+flags.DEFINE_string('label_map_path', '/home/paul/.conda/envs/tensorflow/lib/python3.6/site-packages/tensorflow/models/quiz-w8-data/labels_items.txt',
+                    'Path to label map proto')
+flags.DEFINE_boolean('faces_only', False, 'If True, generates bounding boxes '
+                     'for pet faces.  Otherwise generates bounding boxes (as '
+                     'well as segmentations for full pet bodies).  Note that '
+                     'in the latter case, the resulting files are much larger.')
+FLAGS = flags.FLAGS
+
+SETS = ['train', 'val', 'trainval', 'test']
+YEARS = ['quiz2018', 'merged']
+
+
+#def get_class_name_from_filename(file_name):
+#  """Gets the class name from a file.
+#
+#  Args:
+#    file_name: The file name to get the class name from.
+#               ie. "american_pit_bull_terrier_105.jpg"
+#
+#  Returns:
+#    A string of the class name.
+#  """
+#  match = re.match(r'([A-Za-z_]+)(_[0-9]+\.jpg)', file_name, re.I)
+#  return match.groups()[0]
+
+
+def dict_to_tf_example(data,
+                       mask_path,
+                       label_map_dict,
+                       image_subdirectory,
+                       ignore_difficult_instances=False,
+                       faces_only=True):
+  """Convert XML derived dict to tf.Example proto.
+
+  Notice that this function normalizes the bounding box coordinates provided
+  by the raw data.
+
+  Args:
+    data: dict holding PASCAL XML fields for a single image (obtained by
+      running dataset_util.recursive_parse_xml_to_dict)
+    mask_path: String path to PNG encoded mask.
+    label_map_dict: A map from string label names to integers ids.
+    image_subdirectory: String specifying subdirectory within the
+      Pascal dataset directory holding the actual image data.
+    ignore_difficult_instances: Whether to skip difficult instances in the
+      dataset  (default: False).
+    faces_only: If True, generates bounding boxes for pet faces.  Otherwise
+      generates bounding boxes (as well as segmentations for full pet bodies).
+
+  Returns:
+    example: The converted tf.Example.
+
+  Raises:
+    ValueError: if the image pointed to by data['filename'] is not a valid JPEG
+  """
+  img_path = os.path.join(image_subdirectory, data['filename'])
+  with tf.gfile.GFile(img_path, 'rb') as fid:
+    encoded_jpg = fid.read()
+  encoded_jpg_io = io.BytesIO(encoded_jpg)
+  image = PIL.Image.open(encoded_jpg_io)
+  if image.format != 'JPEG':
+    raise ValueError('Image format not JPEG')
+  key = hashlib.sha256(encoded_jpg).hexdigest()
+
+  width = int(data['size']['width'])
+  height = int(data['size']['height'])
+
+  xmins = []
+  ymins = []
+  xmaxs = []
+  ymaxs = []
+  classes = []
+  classes_text = []
+  truncated = []
+  poses = []
+  difficult_obj = []
+ 
+  for obj in data['object']:
+    difficult = bool(int(obj['difficult']))
+    if ignore_difficult_instances and difficult:
+      continue
+    difficult_obj.append(int(difficult))
+
+    
+    xmin = float(obj['bndbox']['xmin'])
+    xmax = float(obj['bndbox']['xmax'])
+    ymin = float(obj['bndbox']['ymin'])
+    ymax = float(obj['bndbox']['ymax'])
+
+
+    xmins.append(xmin / width)
+    ymins.append(ymin / height)
+    xmaxs.append(xmax / width)
+    ymaxs.append(ymax / height)
+#    class_name = get_class_name_from_filename(data['filename'])
+    classes_text.append(obj['name'].encode('utf8'))
+    classes.append(label_map_dict[obj['name']])
+    truncated.append(int(obj['truncated']))
+    poses.append(obj['pose'].encode('utf8'))
+   
+
+  feature_dict = {
+      'image/height': dataset_util.int64_feature(height),
+      'image/width': dataset_util.int64_feature(width),
+      'image/filename': dataset_util.bytes_feature(
+          data['filename'].encode('utf8')),
+      'image/source_id': dataset_util.bytes_feature(
+          data['filename'].encode('utf8')),
+      'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
+      'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+      'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
+      'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+      'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+      'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+      'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+      'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+      'image/object/class/label': dataset_util.int64_list_feature(classes),
+      'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
+      'image/object/truncated': dataset_util.int64_list_feature(truncated),
+      'image/object/view': dataset_util.bytes_list_feature(poses),
+  }
+
+  example = tf.train.Example(features=tf.train.Features(feature=feature_dict))
+  return example
+
+
+
+def create_tf_record(output_filename,
+                     label_map_dict,
+                     annotations_dir,
+                     image_dir,
+                     examples,
+                     faces_only=True):
+  """Creates a TFRecord file from examples.
+
+  Args:
+    output_filename: Path to where output file is saved.
+    label_map_dict: The label map dictionary.
+    annotations_dir: Directory where annotation files are stored.
+    image_dir: Directory where image files are stored.
+    examples: Examples to parse and save to tf record.
+    faces_only: If True, generates bounding boxes for pet faces.  Otherwise
+      generates bounding boxes (as well as segmentations for full pet bodies).
+  """
+  writer = tf.python_io.TFRecordWriter(output_filename)
+  for idx, example in enumerate(examples):
+    if idx % 100 == 0:
+      logging.info('On image %d of %d', idx, len(examples))
+    xml_path = os.path.join(annotations_dir, 'xmls', example + '.xml')
+    #mask_path = os.path.join(annotations_dir, 'trimaps', example + '.png')
+
+    if not os.path.exists(xml_path):
+      logging.warning('Could not find %s, ignoring example.', xml_path)
+      continue
+    with tf.gfile.GFile(xml_path, 'r') as fid:
+      xml_str = fid.read()
+    xml = etree.fromstring(xml_str)
+    data = dataset_util.recursive_parse_xml_to_dict(xml)['annotation']
+
+    try:
+      tf_example = dict_to_tf_example(
+          data, FLAGS.data_dir, label_map_dict, image_dir, faces_only=faces_only)
+      writer.write(tf_example.SerializeToString())
+    except ValueError:
+      logging.warning('Invalid example: %s, ignoring.', xml_path)
+
+  writer.close()
+
+
+# TODO(derekjchow): Add test for pet/PASCAL main files.
+def main(_):
+  data_dir = FLAGS.data_dir
+  label_map_dict = label_map_util.get_label_map_dict(FLAGS.label_map_path)
+
+  logging.info('Reading from quizdata dataset.')
+  image_dir = os.path.join(data_dir, 'images')
+  annotations_dir = os.path.join(data_dir, 'annotations')
+  examples_path = os.path.join(annotations_dir, 'trainval.txt')
+  examples_list = dataset_util.read_examples_list(examples_path)
+  print('*****************weizhi %s*******'%FLAGS.output_dir)
+  
+  # Test images are not included in the downloaded data set, so we shall perform
+  # our own split.
+  random.seed(42)
+  random.shuffle(examples_list)
+  num_examples = len(examples_list)
+  num_train = int(0.7 * num_examples)
+  train_examples = examples_list[:num_train]
+  val_examples = examples_list[num_train:]
+#  train_examples,val_examples = train_test_split(annotations_dir,FLAGS.label_map_path,0.4,random_state=0)
+  
+  
+  logging.info('%d training and %d validation examples.',
+               len(train_examples), len(val_examples))
+
+  train_output_path = os.path.join(FLAGS.output_dir, 'quizdata_train.record')
+  val_output_path = os.path.join(FLAGS.output_dir, 'quizdata_val.record')
+  
+  create_tf_record(train_output_path, label_map_dict, annotations_dir,
+                   image_dir, train_examples, faces_only=FLAGS.faces_only)
+  create_tf_record(val_output_path, label_map_dict, annotations_dir,
+                   image_dir, val_examples, faces_only=FLAGS.faces_only)
+
+
+if __name__ == '__main__':
+  tf.app.run()
